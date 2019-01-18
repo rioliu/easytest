54fb1f5bb11718677bbf862248665ae9a3a8506f
Rio Liu <rioliu@gmail.com>
2019-01-18 12:32:10 +0800

    Yaml support for configuration context

--- src/main/java/com/rioliu/test/config/TestContext.java
+++ src/main/java/com/rioliu/test/config/TestContext.java
@@ -4,11 +4,18 @@ package com.rioliu.test.config;
 import org.apache.commons.configuration2.EnvironmentConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.SystemConfiguration;
+import org.apache.commons.configuration2.YAMLConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.builder.fluent.PropertiesBuilderParameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.StringUtils;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by rioliu on Nov 13, 2018
@@ -59,6 +66,21 @@ public class TestContext {
         PropertiesConfiguration pc = builder.getConfiguration();
 
         return pc;
+    }
+
+    public YAMLConfiguration loadYamlConfigFromFile(String path) throws ConfigurationException {
+
+        YAMLConfiguration yc = new FileBasedConfigurationBuilder<>(YAMLConfiguration.class)
+                .configure(new Parameters().hierarchical().setFileName(path))
+                .getConfiguration();
+
+        return yc;
+    }
+
+    public <T> T loadYamlConfigObjectFromFile(Class<T> clazz, String path) throws FileNotFoundException {
+
+        Yaml yaml = new Yaml(new Constructor(clazz));
+        return yaml.load(new FileInputStream(path));
     }
 
 }
